8271e67a245ada58e425b748a756d55060f312d8
Eduardo Oliveira Andrade <eduardo.andrade41@etec.sp.gov.br>
2025-04-02 17:46:46 -0300

    Correcao

---
+++ atividade_DS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string op;
+            op = "0";
+            Console.WriteLine("/--MENU---/\n\n Escolhan uma opção:\n\n 1 - opção 1\n 2 - Opção 2\n Q - Sair");
+            while ((op.ToUpper() !="Q"))
+                {
+            Console.WriteLine("Deseja Continuar");
+                op = Console.ReadLine();
+                if (op.Length < 2)
+                {
+                    switch (op)
+                    {
+                        case "1":
+                            Console.WriteLine("Voce escolheu a opção 1");
+                            break;
+                        case "2":
+                            Console.WriteLine("Voce escolheu a opção 2");
+                            break;
+                    }
+                }
+                else { Console.WriteLine("Voce digitou uma opção invalida");
+                    op = "i";
+                }
+            }
+
+        }
+    }
+}
